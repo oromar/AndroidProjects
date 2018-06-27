5b7df09e2f2068c743d0e39ed6c66e3a6d7d44b8
Oromar Lamounier Dantas Melo <oldm@cin.ufpe.br>
2018-06-27 18:38:30 -0300

    Miwok Initial Commit

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        List<String> englishWords = Arrays.asList(new String [] {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"});
+
+        for (String s : englishWords) {
+            Log.v(NumbersActivity.class.getName(), s);
+        }
     }
 }
