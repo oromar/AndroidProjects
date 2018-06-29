b72da8853095484c61377f12c2c6fef6f9c49591
Oromar Lamounier Dantas Melo <oldm@cin.ufpe.br>
2018-06-29 20:35:37 -0300

    ViewPager

--- ViewPager/app/src/main/java/com/example/android/viewpager/MainActivity.java
+++ ViewPager/app/src/main/java/com/example/android/viewpager/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         // Set the content of the activity to use the activity_main.xml layout file
